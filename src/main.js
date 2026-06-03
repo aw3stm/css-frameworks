864149e9370c915cec09ca53c2221860cc8b1e68
Anna Vestman <anna.v.westman@gmail.com>
2026-06-03 10:22:39 +0200

    feat: Create navbar for both the profile and feed page. Implement the logic needed to make the hamnurger menu function correctly.

--- src/main.js
+++ src/main.js
@@ -1,1 +1,34 @@
 import "./style.css";
+
+document.addEventListener("DOMContentLoaded", () => {
+  // ======================================
+  // AUTHENTICATION for login and register
+  // ======================================
+  const loginSection = document.getElementById("login-section");
+  const registerSection = document.getElementById("register-section");
+  const showRegBtn = document.getElementById("show-register-btn");
+  const showLoginBtn = document.getElementById("show-login-btn");
+
+  if (showRegBtn && showLoginBtn) {
+    showRegBtn.addEventListener("click", () => {
+      loginSection.classList.add("hidden");
+      registerSection.classList.remove("hidden");
+    });
+    showLoginBtn.addEventListener("click", () => {
+      registerSection.classList.add("hidden");
+      loginSection.classList.remove("hidden");
+    });
+  }
+  // ======================================
+  // MOBILE MENU
+  // ======================================
+  const mobileBtn = document.getElementById("mobile-btn");
+  const mobileMenu = document.getElementById("mobile-menu");
+
+  if (mobileBtn && mobileMenu) {
+    mobileBtn.addEventListener("click", () => {
+      mobileMenu.classList.toggle("hidden");
+      mobileMenu.classList.toggle("flex");
+    });
+  }
+});

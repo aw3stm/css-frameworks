a7147e70a5a45bc15eeb159554dee9a4e54478a1
Anna Vestman <anna.v.westman@gmail.com>
2026-06-14 18:44:13 +0200

    Installed prettier, husky and ESlint

---
+++ eslint.config.js
@@ -0,0 +1,20 @@
+import js from '@eslint/js';
+
+export default [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        document: 'readonly',
+        window: 'readonly',
+        console: 'readonly',
+      },
+    },
+    rules: {
+      'no-unused-vars': 'warn',
+      'no-console': 'off',
+    },
+  },
+];
